0a28c751fe6d4453a2a35360815a2b93a3a08356
Samandar99 <laziz.go99@gmail.com>
2024-03-18 16:13:35 +0500

    ss

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,28 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { isIOS, isMobile } from "react-device-detect";
 
 const App = () => {
-  const getAppStoreLink = () => {
-    if (isMobile) {
-      return isIOS
-        ? "https://apps.apple.com/us/app/your-app-id"
-        : "https://play.google.com/store/apps/details?id=your.app.package";
-    } else {
-      return "#";
-    }
-  };
+  const [appStoreLink, setAppStoreLink] = useState("#");
 
-  // Определение URL-адреса магазина приложений сразу при загрузке компонента
-  const appStoreLink = getAppStoreLink();
+  useEffect(() => {
+    const getAppStoreLink = () => {
+      if (isMobile) {
+        return isIOS
+          ? "https://apps.apple.com/us/app/your-app-id"
+          : "https://play.google.com/store/apps/details?id=your.app.package";
+      } else {
+        return "#";
+      }
+    };
+
+    // Устанавливаем URL-адрес магазина приложений сразу при загрузке компонента
+    setAppStoreLink(getAppStoreLink());
+  }, []); // Пустой массив зависимостей гарантирует, что эффект выполнится только один раз при загрузке компонента
 
   return (
     <div>
       {/* Используем уже определенный URL-адрес магазина приложений */}
-      <a href={appStoreLink}>
-        {isMobile ? "Download App" : "Download Desktop App"}
-      </a>
+      <a href={appStoreLink}>{isMobile ? "Download App" : "Download Desktop App"}</a>
     </div>
   );
 };
